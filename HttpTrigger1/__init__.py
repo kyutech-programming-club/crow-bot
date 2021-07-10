3a27b7f1d612b05eb21a00c935067418f9b52c36
take-cantik <miki.takeru508@mail.kyutech.jp>
2021-07-10 11:33:21 +0900

    Add Token and secret key

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -11,11 +11,17 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage
 )
 
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+CHANNEL_SECRET = os.getenv('CHANNEL_SECRET')
+
+line_bot = LineBotApi()
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     req_body = req.get_json()
     logging.info(req_body)
+
     return func.HttpResponse(
         "OK",
         status_code=200
